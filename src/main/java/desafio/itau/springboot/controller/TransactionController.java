1dbcf9ac9430c020d016138229ee0ebd44f36e8a
matheusfredo <matheusfredo16@outlook.com>
2025-04-14 18:40:04 -0300

    post /transacao criado e configurado

---
+++ src/main/java/desafio/itau/springboot/controller/TransactionController.java
@@ -0,0 +1,37 @@
+package desafio.itau.springboot.controller;
+
+import desafio.itau.springboot.Service.TransactionService;
+import desafio.itau.springboot.dto.TransactionRequest;
+import desafio.itau.springboot.model.Transaction;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.OffsetDateTime;
+
+@RestController
+@RequestMapping("/transacao")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public ResponseEntity <Void> createTransaction(@Valid @RequestBody TransactionRequest request) {
+        if (request.getDataHora().isAfter(OffsetDateTime.now()) || request.getValor() <= 0) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+        transactionService.addTransaction(new Transaction(request.getValor(), request.getDataHora() ));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity <Void> clearTransactions(){
+        transactionService.clearTransactions();
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/desafio/itau/springboot/dto/TransactionRequest.java
@@ -0,0 +1,21 @@
+package desafio.itau.springboot.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.OffsetDateTime;
+
+public class TransactionRequest {
+
+    private Double valor;
+
+    @NotNull
+    private OffsetDateTime dataHora;
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+}
